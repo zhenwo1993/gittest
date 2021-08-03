fed1ce35e2755ad94cd9293e33bae52d1e6b7703
hezhiyong <hezhiyong@xiaomi.com>
2021-08-03 16:17:27 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/bean/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author hezhiyong
+ * @date 2021-08-03 16:14
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
