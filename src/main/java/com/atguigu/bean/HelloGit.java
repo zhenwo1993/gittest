6e15dc80356f7f60b11a0bda11192ea1cf3f87ea
zhenwo1993 <1123305792@qq.com>
2021-08-03 16:18:27 +0800

    Update HelloGit.java

--- src/main/java/com/atguigu/bean/HelloGit.java
+++ src/main/java/com/atguigu/bean/HelloGit.java
@@ -7,5 +7,6 @@ package com.atguigu.bean;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
